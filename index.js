f8c2212358324544f6f412107633b7eebb056390
jeromevs <56254178+jeromevs@users.noreply.github.com>
2019-12-16 11:24:59 +0100

    set back end on heroku

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/meilleurtaux", {
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -14,6 +18,6 @@ require("./Models/UserProject");
 const userProjectRoutes = require("./routes/userProjectRoute");
 app.use(userProjectRoutes);
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log("Server Ready");
 });

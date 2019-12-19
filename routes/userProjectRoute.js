9f937923a7f6226744393e1390aaa936d190284e
jeromevs <56254178+jeromevs@users.noreply.github.com>
2019-12-19 21:02:53 +0100

    routes get for backoffice

--- routes/userProjectRoute.js
+++ routes/userProjectRoute.js
@@ -22,86 +22,18 @@ router.post("/userProject/save", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-//################### UserProject Create #########
-router.post("/userProject/create", async (req, res) => {
+//###################### GET ALL USERPROJECTS################
+router.get("/userProjects", async (req, res) => {
   try {
-    const newUserProject = new UserProject({
-      typeGood: sanitize.typeGood[req.body.typeGood]
-    });
-    if (!newUserProject.typeGood) {
-      res.json({ message: "invalid request" });
-      return;
-    }
-    const project = await newUserProject.save();
-    res.json({ message: "new userProject created", id: project._id });
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-});
+    const existingUserProjects = await UserProject.find();
 
-//################ UserProject Update ##########
-router.post("/userProject/update", async (req, res) => {
-  try {
-    let id = req.query.id;
-    let typeGood = req.body.typeGood;
-    let stateGood = req.body.stateGood;
-    let usageGood = req.body.usageGood;
-    let userSituation = req.body.userSituation;
-    if (!req.body.locationGood) {
-      req.body.locationGood = {};
+    if (existingUserProjects) {
+      res.json(existingUserProjects);
+    } else {
+      res.status(404).json({ message: "project not found" });
     }
-    let locationGood = {
-      country: req.body.locationGood.country,
-      city: req.body.locationGood.city
-    };
-    if (!req.body.amount) {
-      req.body.amount = {};
-    }
-    let amount = {
-      goodAmount: req.body.amount.goodAmount,
-      workAmount: req.body.amount.workAmount,
-      notaryAmount: req.body.amount.notaryAmount,
-      projectAmount: req.body.amount.projectAmount
-    };
-    let userEmail = req.body.userEmail;
-    let userProjectToUpdate = await UserProject.findById(id);
-    if (typeGood) {
-      userProjectToUpdate.typeGood = typeGood;
-    }
-    if (stateGood) {
-      userProjectToUpdate.stateGood = stateGood;
-    }
-    if (usageGood) {
-      userProjectToUpdate.usageGood = usageGood;
-    }
-    if (userSituation) {
-      userProjectToUpdate.userSituation = userSituation;
-    }
-    if (locationGood.country) {
-      userProjectToUpdate.locationGood.country = locationGood.country;
-    }
-    if (locationGood.city) {
-      userProjectToUpdate.locationGood.city = locationGood.city;
-    }
-    if (amount.good) {
-      userProjectToUpdate.amount.good = amount.good;
-    }
-    if (amount.work) {
-      userProjectToUpdate.amount.work = amount.work;
-    }
-    if (amount.notary) {
-      userProjectToUpdate.amount.notary = amount.notary;
-    }
-    if (amount.project) {
-      userProjectToUpdate.amount.project = amount.project;
-    }
-    if (userEmail) {
-      userProjectToUpdate.userEmail = userEmail;
-    }
-    await userProjectToUpdate.save();
-    res.json(userProjectToUpdate);
   } catch (error) {
-    res.json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
 

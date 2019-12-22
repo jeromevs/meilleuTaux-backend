2e700f57e7a796ec75d31b935ccf5c3ab251e28a
jeromevs <56254178+jeromevs@users.noreply.github.com>
2019-12-22 17:50:36 +0100

    fix a display bug in email

--- routes/userProjectRoute.js
+++ routes/userProjectRoute.js
@@ -27,37 +27,37 @@ router.post("/userProject/save", async (req, res) => {
         "\n" +
         "Nous vous confirmons la reception de votre demande  sous le numero: " +
         newUserProject.fileId +
-        // "\n\n" +
-        // "Type du bien: " +
-        // req.body.typeGood +
-        // "\n" +
-        // "L'etat du bien: " +
-        // req.body.stateGood +
-        // "\n" +
-        // "Votre situation locative actuelle: " +
-        // req.body.userSituation +
-        // "\n" +
-        // "Le montant de votre future acquisition: " +
-        // req.body.amount.good +
-        // " €" +
-        // "\n" +
-        // "Le montant des travaux: " +
-        // req.body.amount.work +
-        // " €" +
-        // "\n" +
-        // "Le montant des frais de notaire extimes: " +
-        // req.body.amount.notary +
-        // " €" +
-        // "\n" +
-        // "Pour un budget total de: " +
-        // req.body.amount.project +
-        // " €" +
-        // "\n" +
-        // "Localise a: " +
-        // req.body.location.city +
-        // "\n" +
-        // "L'equipe de meilleur taux se tient a votre disposition pour toute question supplementaire," +
-        // "\n" +
+        "\n\n" +
+        "Type du bien: " +
+        newUserProject.typeGood +
+        "\n" +
+        "L'etat du bien: " +
+        newUserProject.stateGood +
+        "\n" +
+        "Votre situation locative actuelle: " +
+        newUserProject.userSituation +
+        "\n" +
+        "Le montant de votre future acquisition: " +
+        newUserProject.amount.good +
+        " €" +
+        "\n" +
+        "Le montant des travaux: " +
+        newUserProject.amount.work +
+        " €" +
+        "\n" +
+        "Le montant des frais de notaire extimes: " +
+        newUserProject.notary +
+        " €" +
+        "\n" +
+        "Pour un budget total de: " +
+        newUserProject.project +
+        " €" +
+        "\n" +
+        "Localise a: " +
+        newUserProject.location.city +
+        "\n" +
+        "L'equipe de meilleur taux se tient a votre disposition pour toute question supplementaire," +
+        "\n" +
         "A tres bientot."
     };
     mg.messages().send(data, function(error, body) {
